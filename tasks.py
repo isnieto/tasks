221cc84bc1205bdb41232d74cfa15c9e0f7e7a59
isnieto <ismael.nieto@gmail.com>
2024-05-22 15:41:50 +0200

    translate names

--- tasks.py
+++ tasks.py
@@ -4,16 +4,16 @@ class TaskManager:
         self.tareas_hechas = {}
         self.tareas_borradas = {}
 
-    def add_task(self, id, description):
+    def crear_tarea(self, id, description):
         self.tareas_pendientes[id] = description
 
-    def modify_task(self, id, nueva_descripcion):
+    def modificar_tarea(self, id, nueva_descripcion):
         if id in self.tareas_pendientes:
             self.tareas_pendientes[id] = nueva_descripcion
         elif id in self.tareas_hechas:
             self.tareas_hechas[id] = nueva_descripcion
 
-    def delete_task(self, id):
+    def borrar_tarea(self, id):
         if id in self.tareas_pendientes:
             self.tareas_borradas[id] = self.tareas_pendientes.pop(id)
         elif id in self.tareas_hechas:
@@ -37,7 +37,7 @@ def main():
             
             task_name = input("Por favor, introduzca la tarea: ")
             # Crea nueva tarea
-            manager.add_task(tarea_id, task_name)
+            manager.crear_tarea(tarea_id, task_name)
             print(f'La tarea "{manager.tareas_pendientes[tarea_id]}" ha sido añadida.\n')
             
             # Print all tasks in tareas_pendientes
@@ -63,7 +63,7 @@ def main():
             opcion = int(input("Elija el número: \n"))
             # Delete a task
             if opcion in tareas_list:
-                manager.delete_task(tareas_list[opcion])
+                manager.borrar_tarea(tareas_list[opcion])
             else:
                 print("Invalid option")
         
@@ -79,23 +79,23 @@ def main():
             opcion = int(input("Elija el número: \n"))
             # borra la tarea
             if opcion in tareas_list:
-                manager.delete_task(tareas_list[opcion])
+                manager.borrar_tarea(tareas_list[opcion])
             else:
                 print("Invalid option")
                 
         elif (input_string == '4'):
             print("¿Qué tarea ha sido finalizada?\n")
             counter = 1
-            tareas_hechas = {}
+            tareas_list = {}
             for id, description in manager.tareas_pendientes.items():
                 print(f'{counter}. {description}')
-                tareas_hechas[counter] = id
+                tareas_list[counter] = id
                 counter +=1
             print("\n")
             opcion = int(input("Elija el número: \n"))
             # borra la tarea
-            if opcion in tareas_hechas:
-                manager.finalizar_tarea(tareas_hechas[opcion])
+            if opcion in tareas_list:
+                manager.borrar_tarea(tareas_list[opcion])
             else:
                 print("Invalid option")
                 
